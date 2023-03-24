e9c0f204a098c72d5c42b7206e77091a09afb9ac
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2023-03-23 23:42:44 -0600

    fix(main): correct namespace regex pattern and keep hours
    - Update namespace regex pattern to match 'review' namespaces
    - Set keep hours to 0 to delete namespaces immediately

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ from kubernetes import client, config, watch
  
 config.load_kube_config()
 v1 = client.CoreV1Api()
-ns_regex_pattern = os.getenv("NAMESPACE_REGEX_PATTERN", "^review.*")
-hours = int(os.getenv("KEEP_HOURS", "1"))
-max_namespaces_to_delete = os.getenv("MAX_NAMESPACES_TO_DELETE", None)
-if max_namespaces_to_delete is not None:
-    max_namespaces_to_delete = int(max_namespaces_to_delete)
+ns_regex_pattern = os.getenv("NAMESPACE_REGEX_PATTERN", "^review*")
+hours = int(os.getenv("KEEP_HOURS", "0"))
   
 systemnamespac=["kube-system", "kube-public", "kube-node-lease", "default"] 
 def delete_namespace_resources(namespace):
@@ -28,7 +25,7 @@ while True:
             if namespace.status.phase != 'Terminating':
                 namespaces_to_delete.append(namespace_name)
     namespaces_to_delete.sort(key=lambda x: v1.read_namespace(x).metadata.creation_timestamp)
-    while namespaces_to_delete and (max_namespaces_to_delete is None or deleted_namespaces < max_namespaces_to_delete):
+    while namespaces_to_delete:
         namespace_name = namespaces_to_delete.pop(0)
         try:
             delete_namespace_resources(namespace_name)
